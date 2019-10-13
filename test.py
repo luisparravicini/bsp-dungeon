aaa9fc3b12c28efed0a43d2fed52eb2a63eb09bb
Luis Parravicini <lparravi@gmail.com>
2019-10-12 22:31:12 -0300

    scaled spritesheet instead of screen

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from pygame.locals import *
 import spritesheet
 
 WIN_SIZE = [320, 200]
-SCALE = 2
+SCALE = 3
 TILE_SIZE = 16 * SCALE
 SCREEN_SIZE = [WIN_SIZE[0] * SCALE, WIN_SIZE[1] * SCALE]
 ROOM_SIZE = [int(v / TILE_SIZE) for v in SCREEN_SIZE]
